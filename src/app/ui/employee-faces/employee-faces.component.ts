9b1d0cd9fd450df50bd53e61a3171be6f50954d2
Zbigniew013 <zbigniew.szalkowski@gmail.com>
2022-11-16 14:17:23 +0100

    FB 1.0 add service

---
+++ src/app/ui/employee-faces/employee-faces.component.ts
@@ -0,0 +1,17 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { Observable } from 'rxjs';
+import { EmployeeService } from '../../services/employee.service';
+import { EmployeeModel } from '../../model/employee.model';
+
+@Component({
+  selector: 'app-employee-faces',
+  templateUrl: './employee-faces.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class EmployeeFacesComponent {
+  constructor(private _employeeService: EmployeeService) { }
+
+  data$: Observable<EmployeeModel[] | null> = this._employeeService.getAll();
+
+}
