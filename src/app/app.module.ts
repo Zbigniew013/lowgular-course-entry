cf0711d5b9c8edc32782d19e6ca91ed785083830
Zbigniew013 <zbigniew.szalkowski@gmail.com>
2022-11-16 20:58:17 +0100

    FB 2.0 forms with Extra Challenge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,21 @@ import {EmployeeFacesComponentModule} from "./ui/employee-faces/employee-faces.c
 import {EmployeeServiceModule} from "./services/employee.service-module";
 import {ProjectListComponentModule} from "./ui/project-list/project-list.component-module";
 import {ProjectServiceModule} from "./services/project.service-module";
+import {EmployeeFormComponentModule} from "./ui/employee-form/employee-form.component-module";
 
 @NgModule({
   declarations: [AppComponent],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        HttpClientModule,
-        EmployeeListComponentModule,
-        EmployeeFacesComponentModule,
-        EmployeeServiceModule,
-        ProjectServiceModule,
-        ProjectListComponentModule,
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    EmployeeListComponentModule,
+    EmployeeFacesComponentModule,
+    EmployeeServiceModule,
+    ProjectServiceModule,
+    ProjectListComponentModule,
+    EmployeeFormComponentModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
