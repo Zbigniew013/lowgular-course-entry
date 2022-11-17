6fe7dd15e45676405f239c7cba9e00b0ee0d908d
Zbigniew013 <zbigniew.szalkowski@gmail.com>
2022-11-17 11:09:35 +0100

    FB 2.0 events

--- src/app/ui/employee-form/employee-form.component.ts
+++ src/app/ui/employee-form/employee-form.component.ts
@@ -13,4 +13,7 @@ export class EmployeeFormComponent {
     age: new FormControl(null, [Validators.min(0)]),
     salary: new FormControl(null, [Validators.required, Validators.min(0)])
   });
+  onButtonClicked (form: { name: string, age: number, salary: number }) {
+    alert('New employee ' + form.name + ' , ' + form.age + ' years old was successfully added to the database. Salary: ' + form.salary + ' USD.');
+  }
 }
