1d08a2d9432223e037828d6220c9c037d6cc9f21
Zbigniew013 <zbigniew.szalkowski@gmail.com>
2022-11-18 11:18:40 +0100

    FB 2.0 CRUD, deleted user

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {Observable} from "rxjs";
-import {PersonModel} from "../model/person.model";
-import {CreateEmployeeModel} from "../model/create-employee.model";
+import { Observable } from 'rxjs';
+import { PersonModel } from '../model/person.model';
+import { CreateEmployeeModel } from '../model/create-employee.model';
 
 @Injectable()
 export class EmployeeService {
@@ -16,4 +16,7 @@ export class EmployeeService {
     return this._httpClient.post('https://dummy.restapiexample.com/api/v1/create', employee);
   }
 
+  delete(id: string): Observable<any> {
+    return this._httpClient.delete('https://dummy.restapiexample.com/api/v1/delete/' + id);
+  }
 }
