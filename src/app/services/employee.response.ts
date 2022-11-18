79e23f9290e408a9e54a0becd1e54f6c5b416788
Zbigniew013 <zbigniew.szalkowski@gmail.com>
2022-11-18 18:30:58 +0100

    FB 2.0 response, map response

---
+++ src/app/services/employee.response.ts
@@ -0,0 +1,7 @@
+export interface EmployeeResponse {
+  readonly id: string;
+  readonly employeeName: string;
+  readonly employeeSalary: string;
+  readonly employeeAge: string;
+  readonly profileImage: string;
+}
